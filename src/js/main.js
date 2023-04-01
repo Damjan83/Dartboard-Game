3d64636cba1cc05f26b95f72da75eae5526b23f0
Damjan83 <damjanpokrajac83@gmail.com>
2023-04-01 18:14:56 +0200

    changes

--- src/js/main.js
+++ src/js/main.js
@@ -13,79 +13,101 @@ const contThree = document.querySelectorAll('.container-three div');
 const player = document.querySelectorAll('.player');
 const playBtn = document.querySelector('.start__container-btn');
 const startCont = document.querySelector('.start');
-const scorePlayers = document.querySelectorAll('.pl-score'); 
+const scorePlayers = document.querySelectorAll('.pl-score');
+const typeGame501 = document.getElementById('tg-five');
+const typeGame301 = document.getElementById('tg-three');
+const gameType = document.querySelectorAll('.game-type');
 
 score.innerHTML = 0;
 let click = 0;
 let scoreValue = 0;
 let playerScore = [];
+let lastPlayer = player[player.length - 1];
 
-playBtn.addEventListener('click' , function() {
-    startCont.style.display = 'none';
-});
 
-player.forEach(el => {
+gameType.forEach(el => {
     el.addEventListener('click', function () {
-        if (el.id == 'one') {
-            playerOneScore.textContent = scoreValue;
-            playerScore.push({ id: 'one', score: scoreValue });
-            console.log(playerScore)
-            scoreValue = 0;
-            score.innerHTML = 0;
-            click = 0;
-        } else if (el.id == 'two') {
-            playerTwoScore.textContent = scoreValue;
-            playerScore.push({ id: 'two', score: scoreValue });
-            console.log(playerScore)
-            scoreValue = 0;
-            score.innerHTML = 0;
-            click = 0;
-        } else if (el.id == 'three') {
-            playerThreeScore.textContent = scoreValue;
-            playerScore.push({ id: 'three', score: scoreValue });
-            console.log(playerScore)
-            scoreValue = 0;
-            score.innerHTML = 0;
-            click = 0;
-        } else if (el.id == 'four') {
-            playerFourScore.textContent = scoreValue;
-            playerScore.push({ id: 'four', score: scoreValue });
-            console.log(playerScore)
-            scoreValue = 0;
-            score.innerHTML = 0;
-            click = 0;
-        } else {
-            return;
+        if (el.id == 'tg-five') {
+            scorePlayers.forEach(el => {
+                el.innerHTML = 501;
+            })
+            scoreValue = 0;       
+            typeGame501.classList.add('is-active')
+        }else if (el.id == 'tg-three') {
+            scorePlayers.forEach(el => {
+                el.innerHTML = 301;
+            })
+            score.innerHTML = 301;          
+            typeGame301.classList.add('is-active')
+        }else {
+            console.log('test')
         }
     })
 })
 
+playBtn.addEventListener('click', function () {
+    startCont.style.display = 'none';
+});
+
+// player.forEach(el => {
+//     el.addEventListener('click', function () {
+//         if (el.id == 'one') {
+//             playerOneScore.textContent = scoreValue;           
+//             playerScore.push({ id: 'one', score: scoreValue });
+//             scoreValue = 0;
+//             score.innerHTML = 0;
+//             click = 0;
+//         } else if (el.id == 'two') {
+//             playerTwoScore.textContent = scoreValue;
+//             playerScore.push({ id: 'two', score: scoreValue });
+//             scoreValue = 0;
+//             score.innerHTML = 0;
+//             click = 0;
+//         } else if (el.id == 'three') {
+//             playerThreeScore.textContent = scoreValue;
+//             playerScore.push({ id: 'three', score: scoreValue });
+//             scoreValue = 0;
+//             score.innerHTML = 0;
+//             click = 0;
+//         } else if (el.id == 'four') {
+//             playerFourScore.textContent = scoreValue;
+//             playerScore.push({ id: 'four', score: scoreValue });
+//             scoreValue = 0;
+//             score.innerHTML = 0;
+//             click = 0;
+//         }
+//     })
+// })
+
+
 function clickCheck() {
-    if(click == 3) {
+    if (click == 3) {
         let currentPlayer = document.querySelector('.player.is-active');
         let currentPlayerScore;
 
-        for(let i = 0; i < scorePlayers.length; i++) {
-            if(scorePlayers[i].classList.contains(currentPlayer.id)) {
+        for (let i = 0; i < scorePlayers.length; i++) {
+            if (scorePlayers[i] && scorePlayers[i].classList.contains(currentPlayer.id)) {
                 currentPlayerScore = scorePlayers[i];
             }
         }
-
-        currentPlayerScore.textContent = scoreValue;
+        currentPlayerScore.textContent = currentPlayerScore.textContent - scoreValue;       
         playerScore.push({ id: currentPlayer.id, score: scoreValue });
-        scoreValue = 0;
-        score.innerHTML = 0;
         click = 0;
-        currentPlayer.classList.remove('is-active');
-        currentPlayer.nextElementSibling.classList.add('is-active');
+
+        if (lastPlayer == currentPlayer) {
+            currentPlayer.classList.remove('is-active'); 
+            player[0].classList.add('is-active');
+        }else {     
+            currentPlayer.classList.remove('is-active');   
+            currentPlayer.nextElementSibling.classList.add('is-active');
+        }
     }
 }
-
 
 cont.forEach(el => {
     el.addEventListener('click', function () {
         click += 1;
-        if (click > 3) return;   
+        if (click > 3) return;
         scoreValue += parseInt(el.innerHTML) * 2;
         score.innerHTML = scoreValue;
         localStorage.setItem(score, scoreValue)
